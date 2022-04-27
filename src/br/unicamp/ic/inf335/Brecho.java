b401b1d259f867fc0a90b06d414237ccb332fa88
Marcelo <mg.vasconcelos@terra.com.br>
2022-04-27 20:53:38 -0300

    correção da adicao de itens na lista

--- src/br/unicamp/ic/inf335/Brecho.java
+++ src/br/unicamp/ic/inf335/Brecho.java
@@ -11,10 +11,15 @@ public class Brecho {
 	
 	public static void main(String[] args) {
 		ProdutoBean nProg = new ProdutoBean("CD00001","Celular Galaxy S10", "128 Gb, Preto, com Carregador",1250.0,"Poucos riscos, estado de novo.");
+		produtos.add(nProg);
 		nProg = new ProdutoBean("CD00002","Prod 2 ...", "Bla Bla Bla",1100.0,"Bla Bla Bla");
+		produtos.add(nProg);
 		nProg = new ProdutoBean("CD00003","Prod 3 ...", "Bla Bla Bla",120.0,"Bla Bla Bla");
+		produtos.add(nProg);
 		nProg = new ProdutoBean("CD00004","Prod 4 ...", "Bla Bla Bla",1300.0,"Bla Bla Bla");
+		produtos.add(nProg);
 		nProg = new ProdutoBean("CD00005","Prod 5 ...", "Bla Bla Bla",9400.0,"Bla Bla Bla");
+		produtos.add(nProg);
 		nProg = new ProdutoBean("CD00006","Prod 6 ...", "Bla Bla Bla",1500.0,"Bla Bla Bla");
 		produtos.add(nProg);
 		
